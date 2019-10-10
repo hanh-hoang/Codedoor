a8374cd463573c4cc9716c45bb615def7856ee48
Hanh Hoang <hanh00hoang@gmail.com>
2019-10-10 11:33:18 +0200

    Javascript-Output-ProjectKelvin

---
+++ JS_1_Output.js
@@ -0,0 +1,19 @@
+//Project Kelvin:
+//Create a variable named kelvin, and set it equal to 293. 
+let kelvin = 293;
+
+//Create a variable named Celsius. Subtract 273 minus kelvin and store it in the variable Celsius. 
+let celsius = 373 - kelvin;
+// Create a variable named Fahrenheit, which is calculated from variable Celsius
+let fahrenheit = celsius * (9/5) + 32;
+
+//print variable kelvin
+console.log(kelvin);
+//print variable celsius
+console.log(celsius);
+//print variable fahrenheit
+console.log(fahrenheit);
+
+
+//Find out how to use the .floor() method. 
+console.log(Math.floor(fahrenheit));
